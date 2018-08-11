43ff80ed22fcdfea318fdbb75bb07747a9a87f5e
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2018-08-11 22:52:28 +0300

    Use Mqtt support from js-utils, add types for baconjs

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
-import mqtt = require('mqtt')
 import Bacon = require('baconjs')
 import _ = require('lodash')
-import Client = mqtt.Client
 import btSensorDecode from './BTSensorDecoder'
+import {Mqtt, SensorEvents} from '@chacal/js-utils'
+
+declare module 'baconjs' {
+  interface EventStream<E, A> {
+    slidingTimeWindow<E, A>(windowLengthMs: number): Bacon.EventStream<E, Array<{value: A, timestamp: number}>>;
+  }
+}
+
 
 const MQTT_BROKER = process.env.MQTT_BROKER ? process.env.MQTT_BROKER : 'mqtt://mqtt-home.chacal.fi'
 const MQTT_USERNAME = process.env.MQTT_USERNAME || undefined
 const MQTT_PASSWORD = process.env.MQTT_PASSWORD || undefined
-const MAX_DISCONNECT_COUNT = 20
 
 const BUFFER_TIME_MS = 2000   // Coalesce events from each sensor this many ms and pass on only the one with the highest RSSI
 
-const mqttClient = startMqttClient(MQTT_BROKER, MQTT_USERNAME, MQTT_PASSWORD)
+const mqttClient = Mqtt.startMqttClient(MQTT_BROKER, MQTT_USERNAME, MQTT_PASSWORD)
 mqttClient.subscribe('/bt-sensor-gw/+/value')
 
-Bacon.fromEvent(mqttClient, 'message', (topic, msg) => msg)
+Mqtt.messageStreamFrom(mqttClient)
   .flatMap(parseEventsFromBytes)
   .groupBy(event => event.tag + event.instance)
   .flatMap(groupedStream => {
@@ -29,7 +34,7 @@ Bacon.fromEvent(mqttClient, 'message', (topic, msg) => msg)
   .onValue(e => mqttClient.publish(`/sensor/${e.instance}/${e.tag}/state`, JSON.stringify(e), {retain: true, qos: 1}))
 
 
-function parseEventsFromBytes(message) {
+function parseEventsFromBytes(message): Bacon.EventStream<any, SensorEvents.ISensorEvent> {
   try {
     const messageJson = JSON.parse(message)
     const sensorEvents = btSensorDecode(messageJson.data)
@@ -40,27 +45,9 @@ function parseEventsFromBytes(message) {
   }
 }
 
-function startMqttClient<A>(brokerUrl: string, username: string, password: string): Client {
-  let disconnectCount = 0
 
-  const client = mqtt.connect(brokerUrl, {username, password})
-  client.on('connect', () => console.log('Connected to MQTT server'))
-  client.on('offline', () => {
-    disconnectCount++
-    console.log(`Disconnected from MQTT server. Disconnect count: ${disconnectCount}/${MAX_DISCONNECT_COUNT}`)
-    if(disconnectCount === MAX_DISCONNECT_COUNT) {
-      console.log("Exiting due to too many MQTT disconnects..")
-      process.exit(1)
-    }
-  })
-  client.on('error', (e) => console.log('MQTT client error', e))
-
-  return client
-}
-
-
-Bacon.Observable.prototype.slidingTimeWindow = function(windowDuration) {
-  var addToWindow, now, withTimeStamp
+Bacon.EventStream.prototype.slidingTimeWindow = function(windowDuration) {
+  let addToWindow, now, withTimeStamp
   now = function() {
     return Date.now()
   }
